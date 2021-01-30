894e56791180ae6d0bb9b672a1c9881b60813924
c_melchor <cah.melchor@gmail.com>
2021-01-30 10:44:51 -0800

    server update

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const server = express();
 const recipesRouter = require("./recipes/recipes-router");
+const usersRouter = require("./users/users-router");
 
 server.use(express.json());
 server.use("/api/recipes", recipesRouter);
+server.use("/api/users", usersRouter)
 
 module.exports = server;
 
